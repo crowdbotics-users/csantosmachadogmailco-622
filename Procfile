f647216953e687a91e278d81e6c51fc9b42e3475
Saurav <sp.gharti@gmail.com>
2019-01-04 19:56:35 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT csantosmachadogmailco_622.wsgi:application
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "csantosmachadogmailco-622"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
